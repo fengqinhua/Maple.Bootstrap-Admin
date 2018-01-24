84c960920831715158fbd055f9cd00a1a6d5f58b
fengqinhua <fengqinhua2016@163.com>
2018-01-24 16:09:38 +0800

    初步测试ZUI页面

---
+++ src/static/global/js/app.js
@@ -0,0 +1,16 @@
+/**
+定义页面公共方法的组件
+**/
+;
+var App = function() {
+
+    return {
+        init: function () {            
+        }
+    };
+}();
+
+
+jQuery(document).ready(function() {    
+    App.init(); // 初始化公共组件
+ });
---
+++ src/static/layouts/layout/js/layout.js
@@ -0,0 +1,15 @@
+/**
+处理整个页面布局的组件
+**/
+;
+var Layout = function () {
+
+    return {
+        init: function () {            
+        }
+    };
+}();
+
+jQuery(document).ready(function() {    
+    Layout.init(); // 页面加载时初始化处理整个页面布局的组件
+ });
